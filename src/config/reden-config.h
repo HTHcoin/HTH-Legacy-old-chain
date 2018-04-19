2ffb4cfc97ba7cc86cad8333e7a4f1da47d62a3e
Tri <tri282@gmail.com>
2018-04-19 09:24:39 -0700

    fix reden core verion from 2.0.0 to 1.0.0

--- src/config/reden-config.h
+++ src/config/reden-config.h
@@ -356,7 +356,7 @@
 #define PACKAGE_NAME "Reden Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Reden Core 2.0.0"
+#define PACKAGE_STRING "Reden Core 1.0.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "redencore"
@@ -365,7 +365,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.0.0"
+#define PACKAGE_VERSION "1.0.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
