f601b3be59b71a20a50fae46fb8aecdd33ad7970
Tri <tri282@gmail.com>
2018-04-25 02:19:04 -0700

    change copy right and verioning as well as seeds

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Copyright (c) 2013-2016 The Bitcoin Core developers
-# Copyright (c) 2017-2018 The Reden Core developers
+# Copyright (c) 2017-2018 The Proton Core developers
+# Copyright (c) 2018 The Reden Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
